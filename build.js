91b269b346b14749e5cd900123421078230a904b
Luca C. <hulkandtess@gmail.com>
2024-10-24 11:56:35 +0200

    Update build.js

--- build.js
+++ build.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const userPoolId = process.env.USER_POOL_ID;
 const clientId = process.env.CLIENT_ID;
 
-// Inject the variables into app.js
+// Read the app.js file from the root directory
 let jsContent = fs.readFileSync('./app.js', 'utf8');
 
-// Add global variables for User Pool ID and Client ID at the top of app.js
+// Inject the environment variables at the top of app.js
 jsContent = `const USER_POOL_ID = "${userPoolId}";\nconst CLIENT_ID = "${clientId}";\n` + jsContent;
 
-// Write the modified content back to app.js or to ./public/app.js for deployment
-fs.writeFileSync('./public/app.js', jsContent);
+// Write the modified content back to app.js
+fs.writeFileSync('./app.js', jsContent);
 
 console.log('Environment variables injected into app.js');
